4c2ac65d74c8e95edc30d44e9d1c509690d03ecb
Sofia <sofiia.chuba@student.karazin.ua>
2023-03-22 18:37:33 +0200

    Task four

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(
+                get_bigger_module(200, -8, -30)
+        );
+    }
+
+    public static int get_bigger_module(int number1, int number2, int number3) {
+        int bigger_module;
+
+        if (Math.abs(number1) > Math.abs(number2)) {
+            bigger_module = number1;
+        } else {
+            bigger_module = number2;
+        }
+
+        if (Math.abs(bigger_module) < Math.abs(number3)) {
+            bigger_module = number3;
+        }
+
+        return bigger_module;
     }
 }
